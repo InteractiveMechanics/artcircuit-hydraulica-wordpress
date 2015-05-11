72b40f92df2c5b604fe9e42e470d020d425fc4f5
mtedeschi <mtedeschi@txw6-fysw.accessdomain.com>
2015-05-11 19:31:52 -0400

    Rework lots of stuff, add gitignore, swap in pep.js

--- inc-data.php
+++ inc-data.php
@@ -3,24 +3,30 @@ var data =
 {
     "hotspots": [
         <?php 
+            $count = 0;
             $the_query = new WP_Query( array('post_type' => 'hotspot', 'posts_per_page' => -1) );
         ?>
         <?php if ($the_query->have_posts()): while ($the_query->have_posts()): $the_query->the_post(); ?>
         {
             "id": "<?php print $post->post_name; ?>",
             "category": "stormwater",
+            "image": "<?php the_field('image'); ?>",
             "position": {
-                <?php if (get_field('bottom')): ?>"bottom": "<?php the_field('bottom'); ?>%",<?php endif; ?>
-                <?php if (get_field('left')): ?>"left": "<?php the_field('left'); ?>px"<?php endif; ?>
+                <?php if (get_field('bottom')): ?>"bottom": "<?php the_field('bottom'); ?>px",<?php endif; ?>
+                <?php if (get_field('left')): ?>"left": "<?php the_field('left'); ?>px",<?php endif; ?>
+                <?php if (get_field('z-index')): ?>"zindex": "<?php the_field('z-index'); ?>"<?php endif; ?>
             },
             "size": {
                 <?php if (get_field('width')): ?>"width": "<?php the_field('width'); ?>px",<?php endif; ?>
                 <?php if (get_field('height')): ?>"height": "<?php the_field('height'); ?>px"<?php endif; ?>
             },
             "modal": {
-                <?php if (get_the_content()): ?>"description": <?php print json_encode($post->post_content); ?><?php endif; ?>
+                <?php if (get_the_title()): ?>"title": <?php print json_encode($post->post_title); ?>,<?php endif; ?>
+                <?php if (get_the_content()): ?>"description": <?php print json_encode($post->post_content); ?>,<?php endif; ?>
+                <?php if (get_field('content_image')): ?>"image": "<?php the_field('content_image'); ?>",<?php endif; ?>
+                <?php if (get_field('link')): ?>"link": "<?php the_field('link'); ?>"<?php endif; ?>
             }
-        }
+        }<?php if($count < $the_query->found_posts){ echo ','; } $count++; ?>
         <?php endwhile; endif; wp_reset_postdata(); ?>
     ]
 }
--- index.php
+++ index.php
@@ -27,94 +27,210 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="title" content="<?php wp_title( '|', true, 'right' ); ?>">
     <meta name="copyright" content="Copyright &copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved.">
 
-    <title><?php wp_title( '-', true, 'right' ); ?></title>
+    <title>Hydraulica &ndash; Art on the Circuit</title>
 
     <link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet" type="text/css" />
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/images/icons/apple-touch-icon-180x180.png" />
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/icons/64_favicon.png" sizes="64x64" />
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/icons/32_favicon.png" sizes="32x32" />
     <?php wp_head(); ?>
 </head>
 <body>
+    <?php $post_array = get_pages('pagename = settings'); ?>
+    <?php foreach ( $post_array as $post ) : setup_postdata( $post ); ?>
+        <?php if(get_field('show_alert')): ?>
+            <a class="alert" target="_blank" href="<?php the_field('alert_link'); ?>">
+                <?php the_field('alert_text'); ?>
+                <span class="close">&times;</span>
+            </a>
+        <?php endif; ?>
+    <?php endforeach; wp_reset_postdata();?>
+
+    <a class="menu-link">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve">
+            <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2 S29.104,22,28,22z"/>
+        </svg>
+    </a>
+    <nav id="menu" class="panel" role="navigation">
+        <ul class="panel-inner">
+            <li class="header">Art on the Circuit</li>
+            <?php wp_nav_menu( 
+                array(
+                    'menu'          => 'Sidebar Menu',
+                    'items_wrap'    => '%3$s',
+                    'container'     => ''
+            )); ?>
+        </ul>
+    </nav>
     <div id="wrapper">
         <div class="inner">
-            <div class="clouds"></div>
+            <div class="sky"></div>
             <div class="skyline"></div>
             <div class="trees"></div>
             <div class="foreground">
                 <div class="ground"></div>
-                <div class="water"></div>
+                <div class="river">
+                    <div class="water"></div>
+                    <div class="water"></div>
+                    <div class="water"></div>
+                    <div class="water"></div>
+                    <div class="water"></div>
+                    <div class="water"></div>
+                    <div class="water"></div>
+                </div>
             </div>
             <div class="hotspots"></div>
         </div>
     </div>
     <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
-    <script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery-ui.js"></script>
-    <script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.ui.touch-punch.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.pep.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.cookie.js"></script>
     
     <?php include_once('inc-data.php'); ?>
     <script>
         function buildHotspots(data) {
             $.each(data.hotspots, function(i, val) {
-                var left = (val.position.left ? val.position.left : null);
-                var html =  '<div class="hotspot" id="' + val.id + '"' +
-                            '     style="height:' + val.size.height + '; width:' + val.size.width + '; left:' + left + '; bottom:' + val.position.bottom + ';"' +
-                            '     data-modal="' + val.modal.description + '">' +
-                            '</div>';
+                var link = val.modal.link ? val.modal.link : '';
+                var html =  '<a class="hotspot" id="' + val.id + '"' +
+                            '     style="height:' + val.size.height + '; ' +
+                                        'width:' + val.size.width + '; ' +
+                                        'left:' + val.position.left + '; ' +
+                                        'bottom:' + val.position.bottom + '; ' +
+                                        'z-index:' + val.position.zindex + '; ' +
+                                        'background-image: url(' + val.image + ');" ' +
+                            '     data-modal-content="' + val.modal.description + '" ' +
+                            '     data-modal-title="' + val.modal.title + '" ' +
+                            '     data-modal-image="' + val.modal.image + '" ' +
+                            '     data-modal-link="' + link + '">' +
+                            '     <div class="hotspot-indicator"><?php echo file_get_contents( get_template_directory_uri() . '/images/icon_drops.svg'); ?></div>' +
+                            '</a>';
                 
                 $('.hotspots').append(html);
             });
-        }
-
-        function setMidpoint() {
-            var windowWidth = $(window).width();
-            var $wrapper = $('#wrapper .inner');
-
-            $wrapper.css('left', -(($wrapper.width()/2) - windowWidth/2));
         }
 
         function setDraggable() {
             var windowWidth = $(window).width();
             var $wrapper = $('#wrapper .inner');
 
-            $wrapper.draggable({ 
-                axis: 'x', 
-                drag: function(event, ui) {
-                    if (ui.position.left > 0) {
-                        ui.position.left = 0;
-                    }
-                    if (ui.position.left < -($wrapper.width() - windowWidth)) {
-                        ui.position.left = -($wrapper.width() - windowWidth);
-                    }
-                }
+            var constrainArray = function () {
+                var wDiff = $wrapper.width() - $(window).width();
+                var hDiff = $wrapper.height() - $(window).height();
+                return [
+                    -hDiff,
+                    0,
+                    0,
+                    -wDiff
+                ];
+            };
+
+            var $pep = $wrapper.pep({
+                constrainTo: constrainArray(),
+                axis: 'x',
+                startPos: {
+                    left: -(($wrapper.width()/2) - (windowWidth/2)),
+                    top: null
+                },
+                elementsWithInteraction: '.hotspot'
             });
+        }
+
+        function closeModal() {
+            $('.modal-container').removeClass('in');
+            $('.overlay').removeClass('in');
+            setTimeout(function(){ 
+                $('.modal-container').remove(); 
+                $('.overlay').remove(); 
+            }, 550);
+        }
+        
+        function togglePanel() {
+            $('#menu').toggleClass('active');
+            $('.menu-link').toggleClass('active');
+        }
+
+        function createHotspot(e) {
+            //e.stopPropagation();
+
+            var title = $(this).attr('data-modal-title');
+            var content = $(this).attr('data-modal-content');
+            var link = $(this).attr('data-modal-link');
+            var image = $(this).attr('data-modal-image');
+
+            var html =  '<div class="modal-container">' +
+                        '    <div class="modal">' + 
+                        '        <div class="modal-inner">' +
+                        '            <div class="close">&times;</div>' +
+                        '            <h2>' + title + '</h2>' +
+                        '            <p>' + content + '</p>';
+
+            if (image != 'undefined'){ 
+                html += '            <img src="' + image + '" alt="' + title + '" />'; 
+            }
+            if (link){  
+                html += '            <a href="' + link + '" target="_blank">Learn More &raquo;</a>'; 
+            }
+                html += '        </div>' +
+                        '    </div>' +
+                        '</div>' +
+                        '<div class="overlay"></div>';
+
+            $('body').append(html);
+            setTimeout(function(){ 
+                $('.modal-container').addClass('in'); 
+                $('.overlay').addClass('in');
+            }, 100);
+            //return false;
+        }
+
+        function createEvents() {
+            $('body').on('touchmove', function(e) {
+                if(!e._isScroller) { e.preventDefault(); }
+            });
+
+            $(document).on('click', '.hotspot', createHotspot);
+
+            $(document).on('click', '.modal-container', function(){
+                closeModal();
+            });
+
+            $(document).on('click', '.modal', function(e){
+                e.stopPropagation();
+            });
+            
+            $(document).on('click', '.alert .close', function(e){
+                e.preventDefault();
+                $(this).parent().remove();
+                $.cookie('alert', 'hidden', { expires: 14 });
+            });
+            
+            $(document).on('click', '.modal .close', function(e){
+                e.preventDefault();
+                closeModal();
+            });
+
+            $(document).on('click', '.menu-link', togglePanel);
         }
         
         $(function(){
             buildHotspots(data);
-            setMidpoint();
             setDraggable();
+            createEvents();
             
-            $(document).on('click', '.hotspot', function(){
-                var content = $(this).attr('data-modal');
-                var html =  '<div class="modal-container">' +
-                            '   <div class="modal">' + content + '</div>' +
-                            '   <div class="overlay"></div>' +
-                            '</div>';
-
-                $('body').append(html);
-                setTimeout(function(){ $('.modal-container').addClass('in'); }, 100);
-            });
-
-            $(document).on('click', '.overlay', function(){
-                $('.modal-container').removeClass('in');
-                setTimeout(function(){ $('.modal-container').remove(); }, 550);
-            });
-
-            $('body').on('touchmove', function(e) {
-                if(!e._isScroller) { e.preventDefault(); }
-            });
+            if ($.cookie('alert') == 'hidden'){ $('.alert').remove(); }
         });
     </script>
 
